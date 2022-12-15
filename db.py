cbdea57874cbac46bba40bca9d195d5881b7126c
Evan Tomboulian <etomboulian@gmail.com>
2022-12-14 21:47:09 -0800

    move settings to enums

--- db.py
+++ db.py
@@ -1,26 +1,29 @@
 import pymssql, os
 from datetime import date, datetime
 from dotenv import load_dotenv
+from enum import Enum
 
 load_dotenv()
 
-sql_address = os.environ.get('DB_SERVER')
-sql_port = os.environ.get('DB_PORT')
-sql_db_name = os.environ.get('DB_NAME')
-sql_user = os.environ.get('DB_USER')
-sql_password = os.environ.get('DB_PASSWORD')
-
 query_date_format = '%m/%d/%Y'
-date_format = '%Y.%m.%d'
+output_date_format = '%Y.%m.%d'
+
+class SQLSettings(Enum):
+    sql_address = os.environ.get('DB_SERVER')
+    sql_port = os.environ.get('DB_PORT')
+    sql_db_name = os.environ.get('DB_NAME')
+    sql_user = os.environ.get('DB_USER')
+    sql_password = os.environ.get('DB_PASSWORD')
 
 class Database:
     def __init__(self, export_date: date):
         self.export_date = export_date
-        self.header_row = ['ACTIVENet Daily Agency Fee Export', datetime.now().strftime(date_format), export_date.strftime(date_format)] 
-        # f"declare @export_date date = '03/01/2012'; select 'ACTIVENet Daily Agency Fee Export', convert(varchar,getdate(),102), convert(varchar,@export_date, 102);"
-        self.query_agency_fees_data = """
-        declare @export_date date = '{0}';
-
+        self.header_row = [
+            'ACTIVENet Daily Agency Fee Export', 
+            datetime.now().strftime(output_date_format), 
+            export_date.strftime(output_date_format)
+            ] 
+        self.query_agency_fees_data = """declare @export_date date = '{0}';
         with export_data_detail as (
             select cast(getdate() as date) as today, 
             cast(t.datestamp as date) as tx_date, 
@@ -37,14 +40,19 @@ class Database:
             join GLACCOUNTS gla on ard.GLACCOUNT_ID = gla.GLACCOUNT_ID
             where cast(t.DATESTAMP as date) = @export_date
         )
-
         select convert(varchar,today, 102) as today, convert(varchar, tx_date, 102) as tx_date, account_name, account_number, sum(cc_amount) as cc_amt, sum(cc_fee) as cc_fee, sum(tx_fee) as tx_fee, sum(amt_due_org) as amt_due_org, revenue_site 
         from export_data_detail
         group by today, tx_date, revenue_site, account_name, account_number
         """.format(self.export_date.strftime(query_date_format))
 
     def connect(self):
-        return pymssql.connect(server=sql_address, port=sql_port, user=sql_user, password=sql_password, database=sql_db_name)
+        return pymssql.connect(
+            server=SQLSettings.sql_address.value, 
+            port=SQLSettings.sql_port.value, 
+            user=SQLSettings.sql_user.value, 
+            password=SQLSettings.sql_password.value, 
+            database=SQLSettings.sql_db_name.value
+            )
 
     def execute_query(self, conn, query):
         cursor = conn.cursor()
--- email_ops.py
+++ email_ops.py
@@ -2,26 +2,28 @@ import smtplib, os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from dotenv import load_dotenv
+from enum import Enum
 
 load_dotenv()
 
 # SMTP configuration
-smtp_server = os.environ.get('SMTP_SERVER')
-smtp_port = os.environ.get('SMTP_PORT')
-sender_email = os.environ.get('SENDER_EMAIL')
-sender_password = os.environ.get('SENDER_PASSWORD')
-target_email = os.environ.get('TARGET_EMAIL')
+class SMTPSettings(Enum):
+    smtp_server = os.environ.get('SMTP_SERVER')
+    smtp_port = os.environ.get('SMTP_PORT')
+    sender_email = os.environ.get('SENDER_EMAIL')
+    sender_password = os.environ.get('SENDER_PASSWORD')
+    target_email = os.environ.get('TARGET_EMAIL')
 
 def email_results(file_name):
     # Create the message
     msg = MIMEMultipart()
     msg['Subject'] = '[Email Test]'
-    msg['From'] = sender_email
-    msg['To'] = target_email
+    msg['From'] = SMTPSettings.sender_email.value
+    msg['To'] = SMTPSettings.target_email.value
     msg.attach(MIMEText(open(os.path.join(os.getcwd(), os.environ.get('OUTPUT_PATH'), file_name)).read()))
 
     # send the message
-    with smtplib.SMTP(smtp_server,smtp_port) as server:
+    with smtplib.SMTP(SMTPSettings.smtp_server.value,SMTPSettings.smtp_port.value) as server:
         server.ehlo()
-        server.login(sender_email, sender_password)
-        server.sendmail(sender_email, target_email, msg.as_string())
+        server.login(SMTPSettings.sender_email.value, SMTPSettings.sender_password.value)
+        server.sendmail(SMTPSettings.sender_email.value, SMTPSettings.target_email.value, msg.as_string())
